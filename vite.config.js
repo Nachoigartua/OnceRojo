5491ecc3355f1de4d9350ae5c33001fd4e7c12dc
ignacio <ignacioigartua15@gmail.com>
2025-05-01 01:40:34 -0300

    vite config para publicar a github

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/NOMBRE-DEL-REPO/', // reemplazá con el nombre real de tu repo
+  base: '/OnceRojo/', // reemplazá con el nombre real de tu repo
   plugins: [vue()]
 })
